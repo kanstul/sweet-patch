08837ca6e2c349c5d4ea401631fed57e63afb5db
ask <alex@kanstul.xyz>
2022-06-09 04:43:06 -0700

    Added song history.  Removed legacy message interaction style, has been moved to index.js.legacy_message_commands.js.bak.  Cleaned up a few things, removed some console logs I'll probably end up adding back later.  Basically done.

--- start.js
+++ start.js
@@ -11,11 +11,29 @@ const commands = [
 	new SlashCommandBuilder().setName('kick').setDescription('Sometimes fixes the bot.'),
 	new SlashCommandBuilder().setName('join').setDescription('Joins the voice channel that you\'re in.'),
 	new SlashCommandBuilder().setName('what').setDescription('Responds with currently playing song.'),
+	new SlashCommandBuilder().setName('set').setDescription('Sets the volume.').addNumberOption(option => option.setName('level').setDescription('The new volume level.').setRequired(true)),
+	new SlashCommandBuilder().setName('push').setDescription('Appends song to playlist.').addStringOption(option => option.setName('song').setDescription('The song appended.').setRequired(true)),
+	new SlashCommandBuilder().setName('jump').setDescription('Prepends song to playlist.').addStringOption(option => option.setName('song').setDescription('The song prepended.').setRequired(true)),
+	new SlashCommandBuilder().setName('damp').setDescription('Sets how much the volume is damped when people start talking, send 100 to functionally disable.').addNumberOption(option => option.setName('damp').setDescription('The new damping level.').setRequired(true)),
+	new SlashCommandBuilder().setName('history').setDescription('Responds with a history of all songs played.'),
 	// Use commas. 
 ]
 	.map(command => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(token);
+
+/*
+rest.get(Routes.applicationGuildCommands(clientId, guildId))
+	.then(data => {
+		const promises = [];
+		for (const command of data) {
+			const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
+			promises.push(rest.delete(deleteUrl));
+		}
+		console.log('Deletion complete.');
+		return Promise.all(promises);
+	});
+*/
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Registration complete.'))
